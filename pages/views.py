951754efbcca49995d38a8063f3ffeba118a97cf
Cory <cory.quincy2@gmail.com>
2025-10-02 21:53:09 -0600

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+def home_page_view(request):
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "THAnk you FOR visitING.",
+    }
+    return render(request, "home.html", context)
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
